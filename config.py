abd7e5ea5035e2276ec9754af71701a7e858595b
ikshatelkar <ikshatelkar27@gmail.com>
2026-07-02 17:40:48 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+DATABASE_URL = "postgresql://admin:password123@localhost:5432/prod"
