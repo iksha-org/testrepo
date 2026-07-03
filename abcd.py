5c3ddea7292e83fe7006477f056fbba4465944e6
ikshatelkar <ikshatelkar27@gmail.com>
2026-07-03 15:36:02 +0530

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -1,1 +1,7 @@
-print("HI")
+def calculate(expression):
+    return eval(expression)  # SAST issue: unsafe use of eval()
+
+if __name__ == "__main__":
+    user_input = input("Enter an expression: ")
+    result = calculate(user_input)
+    print(result)
